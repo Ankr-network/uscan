365b7b69016d6437a6fbca1102d46318d0430e70
levtea <rzh2000@163.com>
2022-11-03 13:39:36 +0800

    frontend  request url from browser

---
+++ web/assets/addressService.0b2ccc15.js
@@ -0,0 +1,1 @@
+import{aB as e}from"./index.646e8ac8.js";const r=function(t){return e({url:"/v1/accounts/"+t,method:"get"})};export{r as G};
--- web/assets/addressService.41d7248c.js
+++
@@ -1,1 +0,0 @@
-import{aB as e}from"./index.c5813a35.js";const r=function(t){return e({url:"/v1/accounts/"+t,method:"get"})};export{r as G};
