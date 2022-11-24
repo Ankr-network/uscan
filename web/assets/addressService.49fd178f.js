b159d30e54c530771eb5ce6f2fbaff39e2c812f8
levtea <rzh2000@163.com>
2022-11-24 15:41:51 +0800

    web page fix VITE_BASE_URL

---
+++ web/assets/addressService.49fd178f.js
@@ -0,0 +1,1 @@
+import{k as e}from"./index.c803adf6.js";const r=function(t){return e({url:"/accounts/"+t,method:"get"})};export{r as G};
--- web/assets/addressService.54036c8f.js
+++
@@ -1,1 +0,0 @@
-import{k as e}from"./index.009bcb80.js";const r=function(t){return e({url:"/accounts/"+t,method:"get"})};export{r as G};
