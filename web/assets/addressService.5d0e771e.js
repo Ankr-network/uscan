8ec7675d9de266f4ea01d1a5415405e428fbdd7f
levtea <rzh2000@163.com>
2022-11-25 20:55:26 +0800

    web page txs fix

---
+++ web/assets/addressService.5d0e771e.js
@@ -0,0 +1,1 @@
+import{k as e}from"./index.e25a3bc2.js";const r=function(t){return e({url:"/accounts/"+t,method:"get"})};export{r as G};
--- web/assets/addressService.c8b751ca.js
+++
@@ -1,1 +0,0 @@
-import{k as e}from"./index.498a182e.js";const r=function(t){return e({url:"/accounts/"+t,method:"get"})};export{r as G};
