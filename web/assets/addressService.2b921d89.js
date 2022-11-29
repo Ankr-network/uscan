1e942ad676775ecb70791edeeb302448d14032b1
levtea <rzh2000@163.com>
2022-11-29 17:22:03 +0800

    page fix block overview

---
+++ web/assets/addressService.2b921d89.js
@@ -0,0 +1,1 @@
+import{k as e}from"./index.618b475f.js";const r=function(t){return e({url:"/accounts/"+t,method:"get"})};export{r as G};
--- web/assets/addressService.d9db4094.js
+++
@@ -1,1 +0,0 @@
-import{k as e}from"./index.75f5de9e.js";const r=function(t){return e({url:"/accounts/"+t,method:"get"})};export{r as G};
