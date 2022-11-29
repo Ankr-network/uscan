d4ccf57b43eaced3e5c3407be4799a705afe8ff6
levtea <rzh2000@163.com>
2022-11-29 16:01:58 +0800

    web page bug fix

---
+++ web/assets/addressService.d9db4094.js
@@ -0,0 +1,1 @@
+import{k as e}from"./index.75f5de9e.js";const r=function(t){return e({url:"/accounts/"+t,method:"get"})};export{r as G};
--- web/assets/addressService.e4d2f1c6.js
+++
@@ -1,1 +0,0 @@
-import{k as e}from"./index.14ec23e5.js";const r=function(t){return e({url:"/accounts/"+t,method:"get"})};export{r as G};
