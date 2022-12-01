e296cd5d25d70bf1f53b42e3f3e016e96ec25233
ymwz-max <YM@ankr.com>
2022-12-01 15:50:33 +0800

    fit: merge

--- pkg/apis/api.go
+++ pkg/apis/api.go
@@ -19,13 +19,15 @@ package apis
 import (
 	"context"
 	"fmt"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
+	"github.com/rakyll/statik/fs"
 
 	"github.com/Ankr-network/uscan/pkg/log"
 	"github.com/Ankr-network/uscan/share"
+	_ "github.com/Ankr-network/uscan/statik"
+	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/recover"
-	//_ "github.com/Ankr-network/uscan/statik"
-	"github.com/gofiber/fiber/v2"
 	"github.com/spf13/viper"
 )
 
@@ -36,14 +38,14 @@ func Apis(ctx context.Context) error {
 		DisableStartupMessage: true,
 	})
 
-	//statikFs, err := fs.New()
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//svc.Use("/", filesystem.New(filesystem.Config{
-	//	Root: statikFs,
-	//}))
+	statikFs, err := fs.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	svc.Use("/", filesystem.New(filesystem.Config{
+		Root: statikFs,
+	}))
 
 	g := svc.Group("/uscan/v1")
 	g.Use(recover.New())
