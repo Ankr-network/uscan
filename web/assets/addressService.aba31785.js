755621026db3c11c5c19a12d2bf0a6d3efef5dab
levtea <rzh2000@163.com>
2022-12-02 17:03:55 +0800

    web page verify jump fix

--- web/assets/addressService.aba31785.js
+++
@@ -1,1 +0,0 @@
-import{k as o}from"./index.20bab758.js";const s=function(t){return o({url:"/accounts/"+t,method:"get"})},r=function(t){return o({url:"/accounts/"+t+"/total",method:"get"})};export{r as G,s as a};
