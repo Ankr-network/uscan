3ae3884909cf5e3810406392e51e07af4dabc3ab
levtea <rzh2000@163.com>
2022-12-08 14:34:23 +0800

    web page contract page fix

--- web/assets/addressService.5666562e.js
+++
@@ -1,1 +0,0 @@
-import{k as o}from"./index.eb1a8ca7.js";const s=function(t){return o({url:"/accounts/"+t,method:"get"})},r=function(t){return o({url:"/accounts/"+t+"/total",method:"get"})};export{r as G,s as a};
