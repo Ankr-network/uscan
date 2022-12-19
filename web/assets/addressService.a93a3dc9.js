f8f769b7abe5712d485400901ef5c5d9d4543b26
levtea <rzh2000@163.com>
2022-12-19 11:59:15 +0800

    web page fix

---
+++ web/assets/addressService.a93a3dc9.js
@@ -0,0 +1,1 @@
+import{k as o}from"./index.b2e5404f.js";const s=function(t){return o({url:"/accounts/"+t,method:"get"})},r=function(t){return o({url:"/accounts/"+t+"/total",method:"get"})};export{r as G,s as a};
